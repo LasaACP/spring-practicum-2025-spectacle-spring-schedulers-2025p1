70d05f687132174479017f5d07cc28fc66965fc4
judecarroll99 <alternate@morky.dev>
2025-05-07 09:49:01 -0500

    make compareTasks() use the right size capacity

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -9,14 +9,9 @@ int main() {
 }
 
 bool compareTasks(const Task& a, const Task& b) {
-    int aPeople = 0;
-    int bPeople = 0;
-    for(int i=0;i<sizeof(a.users);i++){
-        aPeople++;
-    }
-    for(int i=0;i<sizeof(b.users);i++){
-        bPeople++;
-    }
+    int aPeople = a.users.size();
+    int bPeople = b.users.size();
+    
     return aPeople > bPeople;
 }
 
