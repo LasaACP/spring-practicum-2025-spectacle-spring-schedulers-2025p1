ea4f26cb66803485f1277cb1c581f9df11b4fc29
Edwin <edwin.c.feng@gmail.com>
2025-05-07 09:23:13 -0500

    HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
-#include "structures.h"
-using namespace std;
+
 int main() {
-    cout << "Hello World!";
-    return 0;
+    vector<Tasks> tasks;
 }
+
+bool compareTasks(const Task& a, const Task& b) {
+    int aPeople = 0;
+    int bPeople = 0;
+    for(int i=0;i<sizeof(a.names);i++){
+        aPeople++;
+    }
+    for(int i=0;i<sizeof(b.names);i++){
+        bPeople++;
+    }
+    return aPeople > bPeople;
+}
+
