396e3756967def51b595552cd60e5985c54c6be0
Edwin <edwin.c.feng@gmail.com>
2025-05-09 09:19:10 -0500

    Created a date system to manage dates that tasks should be done

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -36,3 +36,47 @@ bool compareTasks(const Task& a, const Task& b) {
     return a.duration > b.duration; 
 }
 
+tm getDate(){
+    time_t timestamp;
+    char output[50];
+    struct tm * datetime;
+    time(&timestamp);
+    datetime = localtime(&timestamp);
+    return datetime;
+}
+
+
+tm addDaysToDate(const tm& date, int add) {
+    tm newDate = date;
+    newDate.tm_mday += add;
+    mktime(&newDate);
+    return newDate;
+}
+
+
+void formatDate(const tm& date, char* output, size_t size) {
+    strftime(output, size, "%B %e, %Y", &date);
+}
+
+
+void displaySchedule(const vector<Task>& tasks){
+    vector<Task> sorted = tasks;
+    sort(sorted.begin(), sorted.end(), compareTasks);
+
+
+    cout << "Here's your generated schedule:\n";
+    cout << setw(20) << left << "Task" << setw(15) << "Date" << "Assigned Users\n";
+    cout << "--------------------------------------------------------\n";
+   
+    for (const auto& task : sortedTasks) {
+        cout << setw(20) << left << task.taskName
+             << setw(15) << left << task.date;
+             << "Users: ";
+       
+        for (const auto& user : task.users) {
+            cout << user << " ";
+        }
+        cout << endl;
+    }
+}
+
